f97c80245b5c02db8aee82b0fc7ac3da3f2b8628
narendrapetkar <n.v.petkar@gmail.com>
2017-07-19 20:43:02 +0530

    completing directory struct

---
+++ 14-socketProgramming/simpleSocket/simpleClient.py
@@ -0,0 +1,15 @@
+# this is a simple client program which opens a connection to a given port 12345 and given host. This is very simple to create a socket client using Python's socket module function.
+
+import socket
+
+socketObject = socket.socket()
+
+host = "172.23.23.200"
+
+port = 12345
+
+socketObject.connect((host,port))
+
+print socketObject.recv(1024)
+
+socketObject.close()
