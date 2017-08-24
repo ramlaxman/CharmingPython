9b2ed29f1ba75a11519a159fde4252f66a2e32cd
npetkar <npetkar@Ns-Mac-mini.local>
2017-08-24 12:43:51 +0530

    last exercises

---
+++ solutions/7.py
@@ -0,0 +1,34 @@
+class student:
+    def __init__(self, name, rollno, subjectList):
+        self.name = name
+        self.rollno = rollno
+        self.subjectList = subjectList
+
+    def addSubject(self, subjectName):
+        self.subjectList.append(subjectName)
+
+    def removeSubject(self, subjectName):
+        if len(self.subjecList)!=0:
+            if subjectName in self.subjectList:
+                self.subjectList.remove(subjectName)
+
+class division:
+    def __init__(self, name):
+        self.name = name
+        self.studentList = []
+
+    def addStudent(self, student):
+        self.studentList.append(student)
+
+div1 = division('bca')
+print vars(div1)
+stud1 = student('narendra', 10, ['network, python'])
+print vars(stud1)
+div1.addStudent(stud1)
+print vars(div1)
+stud2 = student('samadhan', 20, ['network, python', 'database'])
+div1.addStudent(stud2)
+print vars(div1)
+
+for student in div1.studentList:
+    print vars(student)
---
+++ solutions/8.py
@@ -0,0 +1,40 @@
+"""
+1. write a program to maintain a list of daily tasks and perform operations on it
+    -- add a new task (avoid repeatations)
+    -- delete a given task (check if the task exists. if not print error message, if list empty, print error)
+    -- delete all tasks (check if list empty)
+    -- show all tasks (check if list empty)
+"""
+
+dailyTasks = []
+
+def printList():
+    if len(dailyTasks) != 0:
+        print dailyTasks
+    else:
+        print "list is empty"
+
+def addNewTask(task):
+    if task in dailyTasks:
+        print "this task already exists"
+        return
+    else:
+        dailyTasks.append(task)
+        return
+
+def removeTask(task):
+    if task not in dailyTasks:
+        print "task unavailable"
+    else:
+        dailyTasks.remove(task)
+
+
+printList()
+addNewTask('wake up')
+printList()
+addNewTask('get ready')
+printList()
+
+removeTask('wake up')
+
+printList()
