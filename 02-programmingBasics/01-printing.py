91f09127a7f52cf90f6f30ee965c071751ed64c7
Mayur Patil <ram.nath241089@gmail.com>
2018-03-13 00:20:44 +0530

    updated for Python3

--- 02-programmingBasics/01-printing.py
+++ 02-programmingBasics/01-printing.py
@@ -1,41 +1,39 @@
 # write different methods of printing and print formatting
 
-print "Hello World"
+print ("Hello World")
 
-print "Hello"
-print "World"
+print ("Hello")
+print ("World")
 
-print "First Line \n Second Line at next position"
+print ("First Line \n Second Line at next position.")  #\n for next line and one space looks like paragraph
 
 paramValue = 20
 
-print "printing variables : %d" % paramValue
-print "printing variables : %s" % paramValue
-print "printing variables : %f" % paramValue
-print "printing variables : %.3f" % paramValue
+# For details, refer ```printf```style formatting [https://docs.python.org/3/library/stdtypes.html#printf-style-string-formatting ]
+print ("printing variables : %d" %paramValue)       # integer
+print ("printing variables : %s" %paramValue)       # 
+print ("printing variables : %f" % paramValue)      # float
+print ("printing variables : %.3f" % paramValue)    # float with decimal values set by user
+print ("printing variables : %g" % paramValue)      # 
 
-print "concatenation of string and variable : ", paramValue
+print ("concatenation of string and variable: ", paramValue)
 
-print "combining string 1 and ", "string 2"
+# Any pair of inverted commas -- which is string disappears after compilation.
+print ("combining string 1 and", 'string 2')
 
-print "double inverted commas", 'single inverted commas'
-
-print "Multi line printing"
-
-print """
+#  #If you are not going to write ```print``` before """, python interpreter will treat it as multiline comment.
+print ("Multi line printing")
+print ("""
 This is the way to print strings that are formatted in code
     -- avoids tabs
     -- avoids next lines
-"""
+""")
 
-complexPrint = ('Put several strings within parentheses '
-        'to have them joined together.')
+complexPrint = ('Put several strings within parentheses'
+                'to have them joined together.')
 
-print complexPrint
+print (complexPrint)
 
-
-# using format string as a variable :
-formatter = "%d.%d.%d.%d"
-print "ip address is : " + formatter % (192, 168, 0, 100)
-
-
+# using format string as a variable : 
+formatter = ("%d.%d.%d.%d")
+print ("ip address is : " + formatter % (192, 168, 0, 100))
