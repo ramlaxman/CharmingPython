f843d94c4c04d61508c4a52a5c6cbc9865c4079a
Mayur Patil <ram.nath241089@gmail.com>
2018-03-13 08:53:12 +0530

    updated for py3

--- 02-programmingBasics/03-arithmatics.py
+++ 02-programmingBasics/03-arithmatics.py
@@ -5,17 +5,17 @@ Example program to understand arithmatics in python
 operand1 = 10
 operand2 = 20
 
-print "addition : %d " % (operand1 + operand2)
-print "substraction : %d" % (operand1 - operand2)
-print "multiplication : %d " % (operand1 * operand2)
-print "division : %d " % (operand1 / operand2)
+print ("Addition : %d " % (operand1 + operand2))
+print ("Substraction : %d" % (operand1 - operand2))
+print ("Multiplication : %d " % (operand1 * operand2))
+print ("division : %d " % (operand1 / operand2))
 
-print "division : %f " % (float(operand1) / float(operand2))
-print "division : %.2f " % (float(operand1) / float(operand2))
+print ("Division : %f " % (float(operand1) / float(operand2)))
+print ("division : %.2f " % (float(operand1) / float(operand2)))
 
-print "5 power 2 ", (5 ** 2)
+print ("5 power 2 ", (5 ** 2))
 
 x = 132.5567/345.22
 
-print "x : ", x
-print "rounding off values : ", round(x, 2)
+print ("x : ", x)
+print ("Rounding off values : ", round(x, 2))
