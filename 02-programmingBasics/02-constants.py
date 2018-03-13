0670e777326ff2ddf8eadc54b3a6f7e0c319c3b5
Mayur Patil <ram.nath241089@gmail.com>
2018-03-13 07:52:19 +0530

    file updated for py3

--- 02-programmingBasics/02-constants.py
+++ 02-programmingBasics/02-constants.py
@@ -3,5 +3,3 @@ print ("this is a constant string")
 # bare numbers are treated as strings
 print (3455)
 print (45.45)
-
-
--- 02-programmingBasics/02-variables.py
+++ 02-programmingBasics/02-variables.py
@@ -5,24 +5,19 @@ Variables are Storage containers. they provide space to store data
 """
 
 var1 = 20
-
 print ("printing variable : ", var1)
 print ("the variable has %d value " % var1)
 
 var1 = 10
-
-print "the value of variable has been reassigned to : ", var1
+print ("The value of variable has been reassigned to : ", var1)
 
 var2 = 30
 
-print "simple arithmatic with variables : "
-print "\tAddition : ", (var1 + var2)
-print "\tSubstraction : ", (var1 - var2)
-print "\tMultiplication : ", (var1 * var2)
-print "\tDivision : ", (var1 / var2)
+print ("Simple arithmatic with variables : ")
+print ("\tAddition : ", (var1 + var2))
+print ("\tSubstraction : ", (var1 - var2))
+print ("\tMultiplication : ", (var1 * var2))
+print ("\tDivision : ", (var1 / var2))
 
 VAR1 = 33
-
-print "case sensitive variable names : %d -- %d " % (var1, VAR1)
-
-
+print ("Same names yet different value due to Case sensitive names : %d -- %d " % (var1, VAR1))
