9c38c98d5cd96e26c5326e40ed4c4562f4b135dd
Mayur Patil <ram.nath241089@gmail.com>
2018-03-14 10:51:00 +0530

    Resolved issue

--- 02-programmingBasics/02-constants.py
+++ 02-programmingBasics/02-constants.py
@@ -1,12 +1,5 @@
 # constants representation of Python
 print ("this is a constant string")
 # bare numbers are treated as strings
-<<<<<<< HEAD
 print(3455)
 print(45.45)
-
-
-=======
-print (3455)
-print (45.45)
->>>>>>> f843d94c4c04d61508c4a52a5c6cbc9865c4079a
