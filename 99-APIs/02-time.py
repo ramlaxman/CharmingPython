fe5763082bbf81a12e88dd2f880559add4d77a11
Mayur Patil <ram.nath241089@gmail.com>
2018-05-04 17:52:23 +0530

    Update 02-time.py

--- 99-APIs/02-time.py
+++ 99-APIs/02-time.py
@@ -3,7 +3,7 @@ A Python program can handle date and time in several ways. Converting between da
 
 Date arithmetic is easy to do with ticks. However, dates before the epoch cannot be represented in this form. Dates in the far future also cannot be represented this way - the cutoff point is sometime in 2038 for UNIX and Windows.
 """
-import time;  # This is required to include time module.
+import time  # This is required to include time module.
 
 ticks = time.time()
 print("Number of ticks since 12:00am, January 1, 1970:", ticks)
