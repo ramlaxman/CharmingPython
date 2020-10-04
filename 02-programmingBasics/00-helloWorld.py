1add888729622406383de8b921795e90c5ae42d0
Ram Patil <linux.craz23@gmail.com>
2020-10-04 23:59:01 +0530

    Update 00-helloWorld.py

--- 02-programmingBasics/00-helloWorld.py
+++ 02-programmingBasics/00-helloWorld.py
@@ -1,8 +1,11 @@
-# write a program to print "Hello World"
+# Write a program to print "Hello World"
+# Most successful program in history of every Programming Language.
 # Give meaningful comments in Python as follows for ex:
+
 # TODO -- in future you have to print ram
 # FIXME -- fix this code later
 
 # If you are using Linux, use following line in each program:
 #!/usr/bin/env python3
+
 print ("Hello World")
